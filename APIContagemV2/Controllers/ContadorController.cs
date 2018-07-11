4b9cb7ff8815ccba828e9518c4383cd2c77757be
Renato Groffe <renato.groffe@yahoo.com.br>
2018-07-11 01:53:01 -0300

    ASP.NET Core 2.1 + Docker + Alpine Linux + Kubernetes + AKS (Azure Kubernetes Service)
    ASP.NET Core 2.1 + Docker + Alpine Linux + Kubernetes + AKS (Azure Kubernetes Service)

---
+++ APIContagemV2/Controllers/ContadorController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIContagemV2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContadorController : ControllerBase
+    {
+        private static Contador _CONTADOR = new Contador();
+
+        [HttpGet]
+        public object Get()
+        {
+            lock (_CONTADOR)
+            {
+                _CONTADOR.Incrementar();
+
+                return new
+                {
+                    _CONTADOR.ValorAtual,
+                    Environment.MachineName,
+                    Sistema = Environment.OSVersion.VersionString
+                };
+            }
+        }
+    }
+}
